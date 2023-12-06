f3d911a1ddbb651fb74c9fc9ec71fc2d4d0ddca5
Wayne Thorley <wayne@waynethorley.co.uk>
2023-12-06 21:46:56 +0000

    frontend: add home page content

--- resources/js/pages/Home.tsx
+++ resources/js/pages/Home.tsx
@@ -4,8 +4,13 @@ import { Page } from '../layout';
 function Home(): React.ReactElement {
     return (
         <Page>
-            <div className="container">
-                <h1 className="text-2xl font-bold">Home</h1>
+            <div className="container my-6 flex-grow flex flex-col items-center justify-center">
+                <h1 className="text-2xl font-bold">
+                    { `Welcome to ${ import.meta.env.VITE_APP_NAME }` }
+                </h1>
+                <p className="mt-2">
+                    Use the links in the navigation to visit the Property &amp; Room pages
+                </p>
             </div>
         </Page>
     );
